7f59a5ba8ea6ef51d3f85997e5767c12c47a98de
Ron Rizzo <churchmouth13@gmail.com>
2022-05-25 15:55:54 -0400

    Add comments to rps.js
    Adds some comments to the bottom of rps.js detailing changes that need
    to be made to game() to work with button input and new game structure.

--- rps.js
+++ rps.js
@@ -101,4 +101,14 @@ function game(){
     }
 }
 
-//game();
+/* game() will start a new game when new game button is pressed
+Global vars:
+winner - string that updates stating the winner of each round, to be used to display results
+pWins - tracks player wins
+cWins - tracks computer wins
+score - string that displays overall score. updates after each round. is reset when game() is ran.
+results - string that displays winner of each round. updates after each round. displays 'CHOOSE' before first selection
+gameOver - a boolean that is true if either player or comp wins = 5.
+
+could use a while loop to run playRound until gameOver is true
+*/
